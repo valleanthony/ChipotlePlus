0889d931bcb7c62a6a58de3cb3739972fc6bd050
Anthony Valle <valleganthony@gmail.com>
2019-04-26 09:37:16 -0400

    Finished adding all items into the array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,6 +13,41 @@ public class Main {
         ArrayList<String> veggie = new ArrayList<>();
 
         //Adding types to each array
+        //Rice first (Ran bound is 2)
+        rice.add("White");
+        rice.add("Brown");
+        rice.add("No Rice");
+
+
+        //Beans (ran bound is 2)
+        beans.add("Pinto");
+        beans.add("Black");
+        beans.add("No Beans");
+
+        //Meat  (Ran bound is 5 )
+        meat.add("Chicken");
+        meat.add("Steak");
+        meat.add("Carnitas");
+        meat.add("Chorizo");
+        meat.add("Sofritas");
+        meat.add("Veggies");
+
+        //Salsa
+        salsa.add("Mild");
+        salsa.add("Medium");
+        salsa.add("Hot");
+        salsa.add("No Salsa");
+        salsa.add("All the Salsa");
+
+        //Veggie
+        veggie.add("Lettuce");
+        veggie.add("Fajita");
+        veggie.add("Veggies");
+        veggie.add("no Veggies");
+        veggie.add("All the veggies");
+
+
+
 
     }
 }
