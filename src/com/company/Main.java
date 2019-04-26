09048bcdd66df2d4a366c4b62e0501a98d786a8d
Anthony Valle <valleganthony@gmail.com>
2019-04-26 09:24:25 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//making five arrays to store the ingredients.
+        ArrayList<String> rice = new ArrayList<>();
+        ArrayList<String> beans = new ArrayList<>();
+        ArrayList<String> meat = new ArrayList<>();
+        ArrayList<String> salsa = new ArrayList<>();
+        ArrayList<String> veggie = new ArrayList<>();
+
+        //Adding types to each array
+
+    }
+}
