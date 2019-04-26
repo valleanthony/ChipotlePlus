d07426bb411de1763adf8c58de236cec6ee93792
Anthony Valle <valleganthony@gmail.com>
2019-04-26 11:55:07 -0400

    Added more options

---
+++ src/com/company/Chip.java
@@ -0,0 +1,100 @@
+package com.company;
+
+public class Chip {
+    String Rice;
+    String Beans;
+    String Meat;
+    String Salsa;
+    String Veggies;
+    String Cheese;
+    String Guac;
+    String SourCream;
+
+    double basePrice = 3.00;
+    double addOn = .50;
+    double PriceofChip;
+
+    public String getRice() {
+        return Rice;
+    }
+
+    public void setRice(String rice) {
+        Rice = rice;
+    }
+
+    public String getBeans() {
+        return Beans;
+    }
+
+    public void setBeans(String beans) {
+        Beans = beans;
+    }
+
+    public String getMeat() {
+        return Meat;
+    }
+
+    public void setMeat(String meat) {
+        Meat = meat;
+    }
+
+    public String getSalsa() {
+        return Salsa;
+    }
+
+    public void setSalsa(String salsa) {
+        Salsa = salsa;
+    }
+
+    public String getVeggies() {
+        return Veggies;
+    }
+
+    public void setVeggies(String veggies) {
+        Veggies = veggies;
+    }
+
+    public String getCheese() {
+        return Cheese;
+    }
+
+    public void setCheese(String cheese) {
+        Cheese = cheese;
+    }
+
+    public String getGuac() {
+        return Guac;
+    }
+
+    public void setGuac(String guac) {
+        Guac = guac;
+    }
+
+    public String getSourCream() {
+        return SourCream;
+    }
+
+    public void setSourCream(String sourCream) {
+        SourCream = sourCream;
+    }
+
+    public Chip() {
+    }
+
+    public Chip(String rice, String beans, String meat, String salsa, String veggies, String cheese, String guac, String sourCream) {
+        Rice = rice;
+        Beans = beans;
+        Meat = meat;
+        Salsa = salsa;
+        Veggies = veggies;
+        Cheese = cheese;
+        Guac = guac;
+        SourCream = sourCream;
+    }
+
+
+
+}
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.beans.Beans;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -15,6 +16,12 @@ public class Main {
         ArrayList<String> meat = new ArrayList<>();
         ArrayList<String> salsa = new ArrayList<>();
         ArrayList<String> veggie = new ArrayList<>();
+        ArrayList<String> cheese = new ArrayList<>();
+        ArrayList<String> guac = new ArrayList<>();
+        ArrayList<String> queso = new ArrayList<>();
+        ArrayList<String> sourcream = new ArrayList<>();
+        ArrayList<String> chipbowl = new ArrayList<>();
+
 
         //Adding types to each array
         //Rice first (Ran bound is 2)
@@ -37,9 +44,9 @@ public class Main {
         meat.add("Veggies");
 
         //Salsa
-        salsa.add("Mild");
-        salsa.add("Medium");
-        salsa.add("Hot");
+        salsa.add("Mild Salsa");
+        salsa.add("Medium Salsa");
+        salsa.add("Hot Salsa");
         salsa.add("No Salsa");
         salsa.add("All the Salsa");
 
@@ -50,12 +57,41 @@ public class Main {
         veggie.add("no Veggies");
         veggie.add("All the veggies");
 
+        //Cheese
+        cheese.add("Cheese");
+        cheese.add("No Cheese");
+
+        //Guac
+        guac.add("Guac");
+        guac.add("No Guac");
+
+        //Queso
+        queso.add("Queso");
+        queso.add("No Queso");
+
+        //Sour Cream
+        sourcream.add("Sour Cream");
+        sourcream.add("No Sour Cream");
+
 
         int ricebound = Rannum.nextInt(rice.size());
         int beanbound = Rannum.nextInt(beans.size());
         int meatbound = Rannum.nextInt(meat.size());
         int salsabound = Rannum.nextInt(salsa.size());
         int veggiebound = Rannum.nextInt(veggie.size());
+        int cheesebound = Rannum.nextInt(cheese.size());
+        int guacbound =Rannum.nextInt(guac.size());
+        int sourcreambound = Rannum.nextInt(sourcream.size());
+
+        String RiceO;
+        String BeansO;
+        String MeatO;
+        String SalsaO;
+        String VeggieO;
+        String CheeseO;
+        String GuacO;
+        String SourcreamO;
+
 
 
         for (int i =1; i<=25; i++){
@@ -65,11 +101,26 @@ public class Main {
                 meatbound = Rannum.nextInt(meat.size());
                 salsabound = Rannum.nextInt(salsa.size());
                 veggiebound = Rannum.nextInt(veggie.size());
+                cheesebound = Rannum.nextInt(cheese.size());
+                guacbound =Rannum.nextInt(guac.size());
+                sourcreambound = Rannum.nextInt(sourcream.size());
             }
             //Is this completely random
             //System.out.println(ricebound + ", "+ beanbound+ ", "+ meatbound + ", "+ salsabound+ ", "+ veggiebound);
-            System.out.println("Burrito " + i+ " : " + rice.get(ricebound)+", "+ beans.get(beanbound)+", "+ meat.get(meatbound) +", "+ salsa.get(salsabound)+", "+ veggie.get(veggiebound) );
+            RiceO = rice.get(ricebound);
+            BeansO = beans.get(beanbound);
+            MeatO = meat.get(meatbound);
+            SalsaO = salsa.get(salsabound);
+            VeggieO = veggie.get(veggiebound);
+            CheeseO = cheese.get(cheesebound);
+            GuacO = guac.get(guacbound);
+            SourcreamO = sourcream.get(sourcreambound);
+            //Chip chippy + i = new Chip(RiceO,BeansO,MeatO,SalsaO,VeggieO,CheeseO,GuacO, SourcreamO)
+            System.out.println("Burrito " + i+ " : " + rice.get(ricebound)+", "+ beans.get(beanbound)+", "+ meat.get(meatbound) +", "+ salsa.get(salsabound)+", "
+                                + veggie.get(veggiebound)+", "+ cheese.get(cheesebound) +", "+guac.get(guacbound) +", "+ sourcream.get(sourcreambound) );
         }
+
+
 
 
     }
